880ebac8d4d1ed641d1a1be158dab77866358058
sangddong <hehe74029@gmail.com>
2024-05-09 02:40:04 +0900

    feat: comments dto 유효성 검사

--- src/domain/comments/comments.dto.ts
+++ src/domain/comments/comments.dto.ts
@@ -1,16 +1,29 @@
-export type CreateCommentsDto = {
+import { Length } from 'class-validator';
+
+export class CreateCommentsDto {
+  @Length(1, 10)
   nickname: string;
-  content: string;
-  password: string;
-};
 
-export type UpdateCommentsDto = {
-  id: number;
+  @Length(1, 50)
   content: string;
-  password: string;
-};
 
-export type DeleteCommentsDto = {
-  id: number;
+  @Length(1, 10)
   password: string;
-};
+}
+
+export class UpdateCommentsDto {
+  id: number;
+
+  @Length(1, 50)
+  content: string;
+
+  @Length(1, 10)
+  password: string;
+}
+
+export class DeleteCommentsDto {
+  id: number;
+
+  @Length(1, 10)
+  password: string;
+}
