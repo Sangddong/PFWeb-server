b7088eb7d6429a1fb806fd023bb24e9ade56a13b
sangddong <hehe74029@gmail.com>
2024-05-09 02:58:49 +0900

    feat: comments service 유효성 검사

--- src/domain/comments/comments.service.ts
+++ src/domain/comments/comments.service.ts
@@ -18,6 +18,9 @@ export class CommentsService {
 
   //댓글 수정 기능
   async editComment(data: UpdateCommentsDto) {
+    await this.findComment(data.id);
+    await this.checkPassword(data.id, data.password);
+
     await this.prismaService.comment.update({
       where: { id: data.id },
       data,
@@ -26,8 +29,30 @@ export class CommentsService {
 
   //댓글 삭제 기능
   async deleteComment(data: DeleteCommentsDto) {
+    await this.findComment(data.id);
+    await this.checkPassword(data.id, data.password);
+
     await this.prismaService.comment.delete({
       where: { id: data.id },
     });
+  }
+
+  //댓글 존재 확인
+  async findComment(commentId: number) {
+    const comment = await this.prismaService.comment.findUnique({
+      where: { id: commentId },
+      select: { password: true },
+    });
+
+    if (!comment) throw new Error('not existing comment');
+
+    return comment;
+  }
+
+  //패스워드 일치 검사
+  async checkPassword(commentId: number, commentPassword: string) {
+    const comment = await this.findComment(commentId);
+
+    if (comment.password !== commentPassword) throw new Error('wrong password');
   }
 }
