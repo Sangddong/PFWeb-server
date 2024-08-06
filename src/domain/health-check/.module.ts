8e624b0bb097bea62c2d5dca0a8a74edbe9fc194
sangddong <hehe74029@gmail.com>
2024-08-06 18:27:22 +0900

    feat: add health check

---
+++ src/domain/health-check/.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { HealthCheckController } from './.controller';
+import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
+
+@Module({
+  controllers: [HealthCheckController],
+  imports: [TerminusModule, HttpModule],
+})
+export class HealthCheckModule {}
